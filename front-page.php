a40be9c4ab0ac1a38b6b6e98a4954a8f41d4c10f
bbarski <bartosz.barski@icloud.com>
2025-05-25 17:08:03 +0200

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -1,14 +1,18 @@
 <?php
 get_template_part('components/header');
+    
+    if ( is_front_page() && ! is_home() ) :
+        get_template_part('components/hero');
+        get_template_part('components/benefits');
+        get_template_part('components/gallery');
+        get_template_part('components/about');
+        get_template_part('components/services');
+        get_template_part('components/testimonials');
 
-if (is_front_page()) :
-    get_template_part('components/hero');
-    get_template_part('components/benefits');
-    get_template_part('components/gallery');
-    get_template_part('components/about');
-    get_template_part('components/services');
-    get_template_part('components/testimonials');
-endif;
+    elseif ( is_home() ):
+        get_template_part('components/blog');
+
+    endif;
 
 get_template_part('components/footer');
 ?>
