5bc26d84b79cf713677b38a1e7c1b9d47887c020
bbarski <biuro@adbito.pl>
2025-10-19 12:03:46 +0200

    Update gallery.php

--- components/gallery.php
+++ components/gallery.php
@@ -3,17 +3,19 @@ $data = get_data_for_template('gallery');
 ?>
 <section class="gallery-section">
 	<div class="galery-container">
-		<h2 class="section-title"><?php echo esc_html($data['gallery_section_title']); ?></h2>
+		<h2 class="section-title">
+			<?php echo esc_html($data['gallery_section_title']); ?>
+		</h2>
 		<div class="gallery">
-				<div class="gallery-item">
-                	<img src="<?php echo esc_html($data['gallery_1_image']); ?>" />
-            	</div>
-				<div class="gallery-item">
-                	<img src="<?php echo esc_html($data['gallery_2_image']); ?>" />
-            	</div>
-				<div class="gallery-item">
-                	<img src="<?php echo esc_html($data['gallery_3_image']); ?>" />
-            	</div>
+<?php
+            foreach ($data as $key => $value) {
+				if (filter_var($value, FILTER_VALIDATE_URL)) {
+            		echo '<div class="gallery-item">';
+        			echo '<img src="' . esc_url($value) . '" alt="' . esc_attr($key) . '" />';
+                	echo '</div>';
+				}	
+            }			
+?>
 		</div>
 		<button></button>
 	</div>
